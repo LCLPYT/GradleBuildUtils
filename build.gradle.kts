354c48198630652ec1cc9b935690efb5bac43c58
LCLP <lukas@lclpnet.work>
2022-01-21 10:51:27 +0100

    first release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties;
+
 plugins {
     java
     `java-gradle-plugin`
@@ -13,20 +15,28 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
+val props = loadPublishProps()
+
+val mavenGroup: String by project
+val mavenArchivesName: String by project
+
 base {
-    group = "work.lclpnet.build"
+    group = mavenGroup
+    archivesName.set(mavenArchivesName)
     version = "1.0.0"
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+
+    withSourcesJar()
 }
 
 gradlePlugin {
     plugins {
         create("gradleBuildUtils") {
-            id = "work.lclpnet.build.gradle-build-utils"
+            id = "gradle-build-utils"
             implementationClass = "work.lclpnet.build.GradleBuildUtilsPlugin"
         }
     }
@@ -34,4 +44,50 @@ gradlePlugin {
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = mavenArchivesName
+            group = mavenGroup
+
+            from(components["java"])
+
+            pom {
+                name.set("gradle-build-utils")
+                description.set("A gradle plugin to help with git versioning and config loading")
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            val env = System.getenv()
+            if (listOf("DEPLOY_URL", "DEPLOY_USER", "DEPLOY_PASSWORD").all(env::containsKey)) {
+                credentials {
+                    username = env["DEPLOY_USER"]
+                    password = env["DEPLOY_PASSWORD"]
+                }
+                url = uri(env["DEPLOY_URL"]!!)
+            }
+            else if (listOf("mavenHost", "mavenUser", "mavenPassword").all(props::containsKey)) {
+                credentials {
+                    username = props.getProperty("mavenUser")
+                    password = props.getProperty("mavenPassword")
+                }
+                url = uri(props.getProperty("mavenHost")!!)
+            } else {
+                url = uri("file:///${project.projectDir}/repo")
+            }
+        }
+    }
+}
+
+fun loadPublishProps(): Properties {
+    val props = Properties()
+    val propsFile = File(project.projectDir, "publish.properties")
+    if (propsFile.exists()) props.load(propsFile.bufferedReader())
+
+    return props
 }
